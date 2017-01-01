9da0e82d9aa26e00e97480f8b784302862874e05
Skyler Bakken <sbakken21@gmail.com>
2017-01-01 15:51:50 +0000

    Attempt at blog

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+class PostsController <ApplicationController
+    before_action :find_post, only: [:show, :edit, :update]
+    def index
+        @posts = Post.all.order("create_at DESC")
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @post.update(post_params)
+        redirect_to @post
+        else
+            render 'edit'
+    end        
+    private
+    
+    def post_params
+        params.require(:post).permit(:name,:email,:comments)
+    end
+    
+    def find_post
+        @post = Post.find(params[:id])
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
     root to: 'pages#home'
-    get 'comments', to: 'pages#comments', as: 'comments'
+    resources :posts
 end
