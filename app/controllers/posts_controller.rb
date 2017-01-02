c40fec6c125bedbdd0c8bb230b6893ad023af379
Skyler Bakken <sbakken21@gmail.com>
2017-01-02 05:19:31 +0000

    Working blog pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,43 +1,24 @@
-class PostsController <ApplicationController
-    before_action :find_post, only: [:show, :edit, :update]
+class PostsController < ApplicationController
     def index
-        @posts = Post.all.order("create_at DESC")
+        @posts = Post.all.order('created_at DESC')
     end
     
     def new
-        @post = Post.new
     end
     
     def create
         @post = Post.new(post_params)
-        if @post.save
-            redirect_to @post
-        else
-            render 'new'
-        end
+        @post.save
+        
+        redirect_to @post
     end
     
     def show
-
-    end
-    
-    def edit
-    end
-    
-    def update
-        if @post.update(post_params)
-        redirect_to @post
-        else
-            render 'edit'
-    end        
-    private
-    
-    def post_params
-        params.require(:post).permit(:name,:email,:comments)
-    end
-    
-    def find_post
         @post = Post.find(params[:id])
     end
     
+    private
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
 end
