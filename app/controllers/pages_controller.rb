0e7e85932969e17b7831a218f11f3244eaff9cbf
Skyler Bakken <sbakken21@gmail.com>
2017-01-02 13:19:03 +0000

    touched up tab and head stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
     def home
     end
+    
+    def temp
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
     root to: 'pages#home'
+    get 'temp', to: 'pages#temp'
     resources :posts do
         resources :comments
     end
