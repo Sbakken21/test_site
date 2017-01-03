465ce15c20dbb1eb6120fade8f4aa6938fb0b738
Skyler Bakken <sbakken21@gmail.com>
2017-01-03 14:30:26 +0000

    Fixed default admin to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103133937) do
+ActiveRecord::Schema.define(version: 20170103142355) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20170103133937) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.boolean  "admin"                   default: false
+    t.boolean  "admin",                  default: false
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
