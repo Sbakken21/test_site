c2fc23bef314a0b4c3c8d632a43687517d86ff40
Skyler Bakken <sbakken21@gmail.com>
2017-01-03 10:06:23 +0000

    Added server coding for subscription plans

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
     root to: 'pages#home'
+    devise_for :users, controllers: { registrations: 'users/registrations' }
     get 'temp', to: 'pages#temp'
     resources :posts do
         resources :comments
