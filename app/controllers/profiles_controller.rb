29231347a89bcfa14bf968425004892861da4842
Skyler Bakken <sbakken21@gmail.com>
2017-01-10 23:04:15 +0000

    Added before filters to get correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
   
   # Get to /users/:user_id/profile/new
   def new
@@ -40,5 +43,10 @@ class ProfilesController < ApplicationController
   private
     def profile_params
       params.require(:profile).permit(:user_name, :first_name, :last_name, :avatar, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
