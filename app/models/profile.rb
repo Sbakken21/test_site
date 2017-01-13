521313340f6d3e24240a3d30e5941b0fef8fc83b
Skyler Bakken <sbakken21@gmail.com>
2017-01-13 18:17:10 +0000

    updated profile layout

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,9 @@ class Profile < ActiveRecord::Base
                     :styles => { :medium => "300x300>", :thumb => "100x100>" },
                     :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
+  has_attached_file :background,
+                    :styles => { :large => "600x600>", :medium => "300x300" },
+                    :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :background, content_type: /\Aimage\/.*\Z/
 end
